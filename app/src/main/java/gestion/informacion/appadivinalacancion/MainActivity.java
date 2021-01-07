f24a7e6185574d9dafeea0521811772d4da8f715
juliarobles <juliarobles@uma.es>
2021-01-07 18:34:35 +0100

    Añadida internacionalización a inglés

--- app/src/main/java/gestion/informacion/appadivinalacancion/MainActivity.java
+++ app/src/main/java/gestion/informacion/appadivinalacancion/MainActivity.java
@@ -6,22 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button btn_jugar;
-    private Button btn_instrucciones;
-    private Button btn_historial;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
         getSupportActionBar().hide();
-        //btn_jugar = (Button) findViewById(R.id.btn_jugar);
-        //btn_instrucciones = (Button) findViewById(R.id.btn_instrucciones);
-        //btn_historial = (Button) findViewById(R.id.btn_historial);
     }
 
     public void respuestaJugar(android.view.View v){
